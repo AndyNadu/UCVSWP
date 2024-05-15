0e6786effa85df908dca4eed1b81c424037d4d28
RRoxana23 <roxanaqs90@gmail.com>
2024-05-15 21:59:16 +0300

    footer deleted

--- UCVSWP/obj/Debug/net7.0/UCVSWP.AssemblyInfo.cs
+++ UCVSWP/obj/Debug/net7.0/UCVSWP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UCVSWP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9fb93b4e4b9f540fc71e572fd718fdf15382ea3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5770142383bde3a6ff18c71258f1127eca15b742")]
 [assembly: System.Reflection.AssemblyProductAttribute("UCVSWP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UCVSWP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
